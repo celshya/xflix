48aca7215e686736261d047a88153212ccc8863e
Celshya D <59332123+celshya@users.noreply.github.com>
2023-03-29 11:02:11 +0530

    Add files via upload

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,2 @@
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb+srv://celshya:Zxcvbnm@qkart-node.mbuazjw.mongodb.net/?retryWrites=true&w=majority" --drop --collection videos --file data/export_xflix_videos.json
